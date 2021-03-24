d48b8087844d348bb31612df40d762e77156503a
Brandon Lamb <blamb888@gmail.com>
2021-03-24 18:10:07 +0900

    add geocoder gem

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,21 @@
 class GroupsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
+  def index
+    @groups = Group.all
+
+    # the `geocoded` scope filters only groups with coordinates (latitude & longitude)
+    @markers = @groups.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
+  end
+
+
+  def group_params
+    params.require(:group).permit(:name, :description, :city)
+  end
+
 end

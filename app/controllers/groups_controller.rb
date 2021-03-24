974f18641e0ebfc388a014d9964db9ec7e7f7a4f
Brandon Lamb <blamb888@gmail.com>
2021-03-24 20:26:36 +0900

    final commit for the night

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,18 +1,18 @@
 class GroupsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!
 
   def index
     @groups = Group.all
 
     # the `geocoded` scope filters only groups with coordinates (latitude & longitude)
-    @markers = @groups.geocoded.map do |group|
-      {
-        lat: group.latitude,
-        lng: group.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { group: group }),
-        image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
-      }
-    end
+    # @markers = @groups.geocoded.map do |group|
+    #   {
+    #     lat: group.latitude,
+    #     lng: group.longitude,
+    #     infoWindow: render_to_string(partial: "info_window", locals: { group: group }),
+    #     image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+    #   }
+    # end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :groups
   resources :users, only: %i[show edit update]
 end

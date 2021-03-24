3dcb807ca2167523ea5f72e76bb1656a71e429f0
Brandon Lamb <blamb888@gmail.com>
2021-03-24 19:27:43 +0900

    map displays on home page. TODO make routes for groups page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import { initMapbox } from '../plugins/init_mapbox';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -27,4 +28,6 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
+
 });

8eea2bfecae2d25a1e51cd299a7f6a6d03bd6c84
Ryugi Suguro <rsuguro@gmail.com>
2021-03-25 23:15:24 +0900

    user/playergroup/groups seed created. Rest still in the works. Will change 'city' => 'address' once updated version is pulled'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Clearing out the battlefield 🌋..."
+User.destroy_all
+PlayerGroup.destroy_all
+Group.destroy_all
+Preference.destroy_all
+Rulebook.destroy_all
+Genre.destroy_all
+puts "Battlefield Cleared ⛰"
+
+puts "Summoning Gandalf 🧙🏻‍♂️"
+gandalf = User.new
+gandalf.email = 'gandalf@gmail.com'
+gandalf.nickname = 'GandalfTheWhite'
+gandalf.password = '123456'
+gandalf.city = 'Tokyo'
+gandalf.save
+puts "Gandalf Summoned: 'You Shall Not Pass! 🧖🏼‍♂️'"
+
+puts "Generating NPCs..."
+40.times do
+  user = User.new
+  user.email = Faker::Internet.email
+  user.nickname = Faker::Internet.username
+  user.password = '123456'
+  user.city = Faker::Address.city
+  user.save
+  puts "#{user.nickname} from #{user.city} created"
+end
+puts "All NPCs Generated"
+
+puts "Generating Groups..."
+50.times do
+  group = Group.new
+  group.name = Faker::App.name
+  group.description = Faker::Company.bs
+  group.city = Faker::Address.city
+  group.gm = User.all.shuffle.first
+  group.save
+  puts "#{group.name} created"
+end
+puts "All Groups Created"
+
+puts "Linking Users to Groups..."
+30.times do
+  p_group = PlayerGroup.new
+  p_group.user = User.all.shuffle.first
+  p_group.group = Group.all.shuffle.first
+  p_group.save
+end
+puts "We have Player Groups 🕺"

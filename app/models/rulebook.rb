e042ca0142237c223a1417d7ef0e165d8f5a764d
Naokimi <49295880+Naokimi@users.noreply.github.com>
2021-03-28 13:12:03 +0000

    removed comments, added relations, vlaidations, and tests

--- app/models/rulebook.rb
+++ app/models/rulebook.rb
@@ -1,6 +1,6 @@
 class Rulebook < ApplicationRecord
   belongs_to :genre
-  # has_many :groups
+  has_many :groups
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, length: { in: 10..500, message: 'Must be between 10 and 500 characters.' }
--- spec/models/player_group_spec.rb
+++ spec/models/player_group_spec.rb
@@ -3,6 +3,31 @@ require 'rails_helper'
 RSpec.describe PlayerGroup, type: :model do
   subject { build(:player_group) }
 
+  context 'valid' do
+    it 'has a valid factory' do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'not valid' do
+    it 'without a user' do
+      subject.user = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'without a group' do
+      subject.group = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'duplicated' do
+      preference = create(:preference)
+      subject.user_id = preference.user_id
+      subject.group_id = preference.group_id
+      expect(subject).not_to be_valid
+    end
+  end
+
   context 'relations' do
     let(:user) { create(:user) }
     let(:group) { create(:group) }

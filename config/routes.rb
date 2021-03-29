5f8d59282c73f094baeef64799e0e72ee2f263cc
Brandon Lamb <blamb888@gmail.com>
2021-03-29 14:50:50 +0900

    change routes for groups

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: %i[show edit update] do
-      resources :groups, only: [:index]
-  end
+  resources :users, only: %i[show edit update]
+  resources :groups, only: [:index]
+
 end

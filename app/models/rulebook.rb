786ff56f51d4cd8df574b4da5a8a258015abb22f
Ryugi Suguro <rsuguro@gmail.com>
2021-03-29 13:02:08 +0900

    rulebook validation message fixed from 300 to 3000

--- app/models/rulebook.rb
+++ app/models/rulebook.rb
@@ -3,6 +3,6 @@ class Rulebook < ApplicationRecord
   # has_many :groups
 
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true, length: { in: 10..3000, message: 'Must be between 10 and 300 characters.' }
+  validates :description, presence: true, length: { in: 10..3000, message: 'Must be between 10 and 3000 characters.' }
   validates :img_url, presence: true, format: { with: /\.(png|jpg|jpeg|svg)\Z/i }
 end

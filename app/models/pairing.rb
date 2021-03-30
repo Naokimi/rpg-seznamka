012c89f0487236a5c33b5de814e65790f4df56ea
Chiara Campalani <campalanichiara@gmail.com>
2021-03-30 23:42:50 +0900

    validations and tests for pairing model

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -1,4 +1,7 @@
 class Pairing < ApplicationRecord
   belongs_to :genre
   belongs_to :rulebook
+
+  validates :genre, presence: true
+  validates :rulebook, presence: true, uniqueness: { scope: :genre }
 end

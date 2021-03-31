d802db5e2b2b4e2d539fca249f91d171b9ade3a5
Chiara Campalani <campalanichiara@gmail.com>
2021-03-31 23:33:37 +0900

    updated pairing model validations

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -2,6 +2,5 @@ class Pairing < ApplicationRecord
   belongs_to :genre
   belongs_to :rulebook
 
-  validates :genre, presence: true
-  validates :rulebook, presence: true, uniqueness: { scope: :genre }
+  validates :rulebook, uniqueness: { scope: :genre }
 end

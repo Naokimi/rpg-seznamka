f5719866363ce54e536a9a7997d48838424cfadb
Chiara Campalani <campalanichiara@gmail.com>
2021-04-23 23:22:57 +0900

    added event listeners to toggle availability selection

--- app/javascript/packs/availability-grid.js
+++ app/javascript/packs/availability-grid.js
@@ -5,45 +5,52 @@ function generateTableRows(parent) {
     const row = document.createElement('div');
     parent.appendChild(row);
     row.id = 'row' + i;
-    row.className = 'row';
+    row.className = 'availabilityRow';
   }
 };
 generateTableRows(availabilityBox);
 
 // generating table's column headers
 const rowHeader = document.getElementById('row-1');
-const week = ['', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const week = ['', 'M', 'T', 'W', 'T', 'F', 'S', 'S'];
 function generateColumnHeaders(row) {
   for (let i=0; i<=7; i++) {
     const header = document.createElement('div');
     row.appendChild(header);
     header.innerHTML = week[i];
-    header.className = 'columnHeader';
+    header.className = 'columnHeader box';
   }
 }
 generateColumnHeaders(rowHeader);
 
 // generating availability grid
+let rowIndex = 0;
 function generateAvailability(row) {
-  let rowIndex = 0;
   for (let i=0; i<=7; i++) {
     const availabilityBox = document.createElement('div');
+    const innerSquare = document.createElement('div');
     row.appendChild(availabilityBox);
     if (i === 0) {
       availabilityBox.innerHTML = rowIndex;
-      availabilityBox.className = 'rowHeader';
+      availabilityBox.className = 'rowHeader box';
       //availabilityBox.id = params-attributes
       rowIndex += 1;
     } else {
-      availabilityBox.className = 'availabilityBox';
+      availabilityBox.className = 'availabilityBox box';
+      availabilityBox.appendChild(innerSquare);
+      innerSquare.className = 'square';
     }
   }
 };
 
-const rows = document.getElementsByClassName('row');
-[...rows].forEach(row => {
-  console.log(row);
-  // if (row !== 'row-1') {
-  // }
-})
+const rows = document.getElementsByClassName('availabilityRow');
+const rowsArray = [...rows];
+rowsArray.shift();
+rowsArray.forEach(row => generateAvailability(row));
 
+const box = document.getElementsByClassName('availabilityBox');
+[...box].forEach(box => {
+  box.addEventListener('mousedown', e => {
+    e.target.classList.toggle('available');
+  } )
+});
